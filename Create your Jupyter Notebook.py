d6d7befe7c2d8985212be7724050025f732d9ebb
artur8108 <117227842+artur8108@users.noreply.github.com>
2022-11-10 14:17:35 -0600

    Add files via upload

---
+++ Create your Jupyter Notebook.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Artur Rodrigues**  
+# 
+# Data Analyst
+
+# *I am interested in data science because it opens new doors in the industry for me. I can leverage the gained knowledge in it along with my MBA to change career paths* 
+
+# ### Simple list length
+
+# In[9]:
+
+
+x= [1, 3, 5, 7, 9];
+print (len(x))
+
+
+# <strike>There are even numbers within x<strike>
+
+# --- 
+
+# https://www.splashlearn.com/math-vocabulary/number-sense/even-and-odd-numbers
+
+# 1. Oh
+# 2. We
+# 3. Are
+# 4. Done
